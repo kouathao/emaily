146e96a36c710d309571fdea2436eef8e3609c99
Koua Thao <kouathao09@gmail.com>
2019-05-04 19:29:43 -0500

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,17 @@
+// production keys here
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+// production db
+// mongodb+srv://kouathao09:LovingU09!@@emaily-prod-j8eep.mongodb.net/test?retryWrites=true
+
+// production creds googleAPI
+// 1012836740235-df0d7qpqmlfudia6b0l9fl423qb1piq1.apps.googleusercontent.com
+
+// client secret keys
+// KT7Geutqm1x3acxq57EJQMb0
