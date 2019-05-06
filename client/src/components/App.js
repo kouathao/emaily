36fc2549ff76fdbd2e6e26702cec9c40f3b2aa3d
Koua Thao <kouathao09@gmail.com>
2019-05-06 01:04:59 -0500

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+import Header from "./Header";
+import Landing from "./Landing";
+
+const Dashboard = () => <h4>Dashboard</h4>;
+const SurveyNew = () => <h4>SurveyNew</h4>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
